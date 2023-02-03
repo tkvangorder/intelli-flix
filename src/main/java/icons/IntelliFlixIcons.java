00730d9930dbcc26ad3a53935c94396ebd6d4fe3
Tyler Van Gorder <tkvangorder@users.noreply.github.com>
2023-02-03 14:30:30 -0800

    Stub out tools menu and actions.

---
+++ src/main/java/icons/IntelliFlixIcons.java
@@ -0,0 +1,16 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * Icons used by the plugin.
+ * <P>
+ * TODO: Move this under the plugin package, the documentation says this is allowed, but I was not able to get this working
+ *
+ */
+@SuppressWarnings("unused")
+public interface IntelliFlixIcons {
+    Icon IntelliFlix = IconLoader.getIcon("/icons/intelliflix.png", IntelliFlixIcons.class);
+}
---
+++ src/main/java/org/tkv/intelliflix/actions/OpenChatAction.java
@@ -0,0 +1,12 @@
+package org.tkv.intelliflix.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class OpenChatAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
